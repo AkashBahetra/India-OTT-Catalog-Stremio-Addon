af28987eaa075524d8d2cd39c5e2c94281b9a0a1
Akash bahetra <akashbahetra05@gmail.com>
2025-11-08 14:19:26 +0530

    fix

--- addon.js
+++ addon.js
@@ -555,6 +555,20 @@ setTimeout(async () => {
 
 const app = express();
 
+// Add CORS headers for Stremio
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    
+    // Handle preflight
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+    }
+    
+    next();
+});
+
 // Serve configuration page
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'configure.html'));
